2e6c2077918199b0e5a7a221ba1149d5025c0c2d
Derrick Vuong <derrick.vuong27@gmail.com>
2020-06-30 11:04:31 +1000

    Initial commit

---
+++ src/lwc/qsydFileExplorerResults/qsydFileExplorerResults.js
@@ -0,0 +1,78 @@
+/**
+ * @File Name          : qsydFileExplorerResults.js
+ * @Description        :
+ * @Author             : Paul Lucas, Jiun Ryu, Elly Zhu, Derrick Vuong
+ * @Group              :
+ * @Last Modified By   : Derrick Vuong
+ * @Last Modified On   : 4/29/2020, 12:14:38 PM
+ * @Modification Log   :
+ * Ver       Date            Author      		    Modification
+ * 1.0       4/23/2020       PL, JR, EZ, DV         Initial Version
+ **/
+import {LightningElement, api} from 'lwc';
+
+export default class QsydFileExplorerResults extends LightningElement {
+	@api recordId;
+	@api selectedItem;
+	@api results;
+	@api breadcrumbs;
+
+	constructor() {
+		super();
+	}
+
+	connectedCallback() {
+	}
+
+	renderedCallback() {
+	}
+
+	// #### CONTENT GETTERS
+	get contentsRequested() {
+		return this.results && this.results.type == 'Contents';
+	}
+
+	get selectedItemId() {
+		return this.selectedItem ? this.selectedItem.id : null;
+	}
+
+	get folderId() {
+		if (this.selectedItem) {
+			return this.selectedItem.documentId
+				? this.selectedItem.folder
+				: this.selectedItem.id;
+		}
+		return null;
+	}
+
+	// #### SEARCH GETTERS
+	get activeSections() {return ['Files', 'Folders', 'Tags']; }
+
+	get searchRequested() {
+		return this.results && this.results.type == 'Search';
+	}
+
+	get filesAvailable() { return this.results.files.length > 0; }
+
+	get foldersAvailable() { return this.results.folders.length > 0; }
+
+	get tagsAvailable() { return this.results.tags.length > 0; }
+
+	get filesLabel() { return 'Files (' + this.results.files.length + ')'; }
+
+	get foldersLabel() {
+		return 'Folders (' + this.results.folders.length + ')';
+	}
+
+	get tagsLabel() { return 'Tagged (' + this.results.tags.length + ')'; }
+
+	handleClick(e) {
+		const selectedItem = e.detail;
+		//this.selectedItemId = selectedItem.id;
+		this.dispatchEvent(
+			new CustomEvent('itemclick', {
+				detail: selectedItem,
+			}),
+		);
+	}
+}
